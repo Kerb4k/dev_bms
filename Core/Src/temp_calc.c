05ace47fdc968b0dd8690b44d2128c3b79df72d5
Kerb4k <dovlat2003@gmail.com>
2023-07-20 15:59:51 +0300

    new temp calc

--- Core/Src/temp_calc.c
+++ Core/Src/temp_calc.c
@@ -9,26 +9,41 @@
 
 
 
-void temp_calc(uint8_t total_ic,  temp_data_t temp_data[][GPIO_NUM]){ //fix arguments
-	float v, r, vv , t;
-	for(int i = 0; i < total_ic; i++){
-		for(int j = 0; j < GPIO_NUM; j++){
-			//vv =  ic[i].aux.a_codes[j];
-			vv = temp_data[i][j].raw;
-			v = vv/10000;
-			r = (v*10000)/(3-v);
-			t = log(r/10000);
-			    t = t / 3660;
-			    t = t + 1/298.15;
-			    t = 1/t;
+void temp_calc(uint8_t total_ic,  temp_data_t temp_data[][GPIO_NUM]){
+    float v, r, vv , t;
+    float B = 3730; // B-parameter from CSV file
+    float R0 = 10000; // Reference resistance at 25 degrees Celsius
+    float T0 = 298.15; // Reference temperature in Kelvin (25 degrees Celsius)
+    float tolerance = 0.05; // Tolerance of the thermistor
 
-			    t -= 273.15;
-		temp_data[i][j].temp = (int)t;
-		//	ic[i].aux.s_temp[j] = t;
-
-		}
-	}
-
+    for(int i = 0; i < total_ic; i++){
+        for(int j = 0; j < 5; j++){
+            vv = temp_data[i][j].raw;
+            v = vv/10000;
+            r = (v*10000)/(3-v);
+            r = r * (1 + tolerance); // Adjust resistance for tolerance
+            t = log(r/R0);
+            t = t / B;
+            t = t + 1/T0;
+            t = 1/t;
+            t -= 273.15; // Convert from Kelvin to Celsius
+            temp_data[i][j].temp = (int)t;
+        }
+    }
+    for(int i = 0; i < total_ic; i++){
+    	for(int j = 6; j < 9; j++){
+    		vv = temp_data[i][j].raw;
+            v = vv/10000;
+            r = (v*10000)/(3-v);
+            r = r * (1 + tolerance); // Adjust resistance for tolerance
+            t = log(r/R0);
+            t = t / B;
+            t = t + 1/T0;
+            t = 1/t;
+            t -= 273.15; // Convert from Kelvin to Celsius
+            temp_data[i][j].temp = (int)t;
+    	}
+    }
 }
 
 #endif
