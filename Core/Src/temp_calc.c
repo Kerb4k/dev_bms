d3d77528bcefa8ca1fefcbe5ea6c86e5dd949bb2
Kerb4k <dovlat2003@gmail.com>
2023-07-21 18:04:47 +0300

    new temp calc

--- Core/Src/temp_calc.c
+++ Core/Src/temp_calc.c
@@ -12,13 +12,14 @@
 void temp_calc(uint8_t total_ic,  temp_data_t temp_data[][GPIO_NUM]){
     float v, r, t;
     float B = 3730; // B-parameter from CSV file
-    float R0 = 10000; // Reference resistance at 25 degrees Celsius
+    float R0 = 2700; // Reference resistance at 25 degrees Celsius
     float T0 = 298.15; // Reference temperature in Kelvin (25 degrees Celsius)
-    float Vs = 3; // Source voltage in volts Vref2
-    float R1 = 10000; // Resistance of the voltage divider in ohms
+
+    float R1 = 2700; // Resistance of the voltage divider in ohms
 
     for(int i = 0; i < total_ic; i++){
         for(int j = 0; j < 6; j++){
+        	float Vs = (float)temp_data[i][5].raw; // Source voltage in volts Vref2
             v = temp_data[i][j].raw / 10000; // Convert raw reading to volts
             r = (v * R1) / (Vs - v); // Calculate resistance of the thermistor
             t = log(r/R0);
@@ -32,6 +33,7 @@ void temp_calc(uint8_t total_ic,  temp_data_t temp_data[][GPIO_NUM]){
 
     for(int i = 0; i < total_ic; i++){
     	for(int j = 6; j < 10; j++){
+    		float Vs = (float)temp_data[i][5].raw; // Source voltage in volts Vref2
     		v = temp_data[i][j].raw / 10000; // Convert raw reading to volts
     		r = (v * R1) / (Vs - v); // Calculate resistance of the thermistor
     		t = log(r/R0);
